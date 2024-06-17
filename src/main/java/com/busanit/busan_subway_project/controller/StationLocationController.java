6279d0d4f393066289a7140e807c56e169f2b57c
moonyoungmin <ansdudals8@naver.com>
2024-06-17 15:20:49 +0900

    station_location get 메서드

---
+++ src/main/java/com/busanit/busan_subway_project/controller/StationLocationController.java
@@ -0,0 +1,20 @@
+package com.busanit.busan_subway_project.controller;
+
+import com.busanit.busan_subway_project.dao.StationLocationDao;
+import com.busanit.busan_subway_project.model.Station_Location;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StationLocationController {
+    @Autowired
+    private StationLocationDao stationLocationDao;
+
+    @GetMapping("stationlocation/{scode}")
+    public Station_Location getSLByScode(@PathVariable Long scode){
+        return stationLocationDao.getStationLocation(scode);
+    }
+
+}
--- src/main/java/com/busanit/busan_subway_project/repo/StationRepo.java
+++ src/main/java/com/busanit/busan_subway_project/repo/StationRepo.java
@@ -6,4 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface StationRepo extends JpaRepository<Station, Integer> {
     // Station 의 PK는 scode, Integer 타입
     Station findByScode(Integer scode);
+
+    // Station의 sname 으로 station 전체 찾기
+    Station findBySname(String sname);
 }
