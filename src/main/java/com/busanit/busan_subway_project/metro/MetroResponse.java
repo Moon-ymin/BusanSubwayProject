204e9df3be22ca77b80bc5a726bfa782e51aba3a
moonyoungmin <ansdudals8@naver.com>
2024-06-20 00:12:31 +0900

    지하철 OpenAPI 활용 DB 데이터 접근

--- src/main/java/com/busanit/busan_subway_project/metro/MetroResponse.java
+++ src/main/java/com/busanit/busan_subway_project/metro/MetroResponse.java
@@ -1,19 +1,48 @@
 package com.busanit.busan_subway_project.metro;
 
+import com.busanit.busan_subway_project.model.Metro;
 import com.google.gson.annotations.SerializedName;
-import org.apache.catalina.connector.Response;
+import lombok.Data;
 
 import java.util.List;
 
+@Data
 public class MetroResponse {
-    public Response response;
 
+    @SerializedName("response")
+    private Response response;
+
+    @Data
     public static class Response {
-        public Body body;
+        @SerializedName("header")
+        private Header header;
+
+        @SerializedName("body")
+        private Body body;
+
+    }
+    @Data
+    public static class Header {
+        @SerializedName("resultCode")
+        private String resultCode;
+
+        @SerializedName("resultMsg")
+        private String resultMsg;
     }
 
+    @Data
     public static class Body {
         @SerializedName("item")
-        public List<Metro> itemList;
+        private List<Metro> itemList;
+
+        @SerializedName("numOfRows")
+        private int numOfRows;
+
+        @SerializedName("pageNo")
+        private int pageNo;
+
+        @SerializedName("totalCount")
+        private int totalCount;
     }
 }
+
